e5facba26270ef5b089a5fc755b86b6a8335e5fb
rodrigoRolim <rodrigorolimveras92@gmail.com>
2025-10-27 20:23:04 -0300

    create service to estimate training time

---
+++ services/time_estimator_service.py
@@ -0,0 +1,51 @@
+import math
+from models import RideParameters
+from services.parse_files_service import parse_tcx_to_segmented_route
+from services.zone_service import get_power_by_zone_name
+from services.power_service import get_steady_state_velocity
+from utils import format_time
+
+
+def estimate_training_time_required_to_route(ride_parameters: RideParameters):
+  """Estimate training time required to complete a route"""
+  total_mass = ride_parameters.rider_mass + ride_parameters.bike_mass
+  total_time_sec = 0
+  total_dist_m = 0
+  details = []
+
+  for i, segment in enumerate(ride_parameters.route_segments):
+    slope = math.atan(segment.grade / 100)
+    power = get_power_by_zone_name(segment.zone, ride_parameters.ftp)
+
+    wind_component = 0
+    if segment.heading is not None:
+      wind_component = ride_parameters.wind_speed / 3.6 * math.cos(math.radians(segment.heading - ride_parameters.wind_dir))
+
+    rider_velocity = get_steady_state_velocity(
+      power, 
+      ride_parameters.air_density, 
+      ride_parameters.cda, 
+      ride_parameters.cr, 
+      wind_component, 
+      total_mass, 
+      ride_parameters.gravity, 
+      slope
+    )
+
+    time_segment_sec = segment.distance / rider_velocity
+    total_time_sec += time_segment_sec
+    total_dist_m += segment.distance
+
+    details.append({
+      "segment": i + 1, # util to create the workout file
+      "distance_km": segment.distance / 1000,
+      "grade_percent": segment.grade,
+      "zone": segment.zone,  # util to create the workout file
+      "power_w": power, # util to create the workout file
+      "speed_kmh": rider_velocity * 3.6,
+      "time_min": time_segment_sec / 60 # util to create the workout file
+    })
+
+  total_time_h_m = format_time(total_time_sec)
+  total_dist_km = total_dist_m / 1000
+  return total_time_h_m, total_dist_km, details
