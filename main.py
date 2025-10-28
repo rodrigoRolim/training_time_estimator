d326d8fbd945c1878fa310f36817f19b83b81f4f
rodrigoRolim <rodrigorolimveras92@gmail.com>
2025-10-28 13:19:47 -0300

    rename routes to 'api'

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
-from routes import time_estimator
+from routes import api
+import logging
+
+# Configuração global do logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+
+logger = logging.getLogger()  # root logger
 
 app = FastAPI(title="Workout generator Service")
 
-app.include_router(time_estimator.router, prefix="/workout", tags=["items"])
+app.include_router(api.router, prefix="/workout")
 
 @app.get("/")
 def health_check():
