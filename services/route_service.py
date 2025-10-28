a2ae4dc6e32c8838008b9cebb02f31802803105a
rodrigoRolim <rodrigorolimveras92@gmail.com>
2025-10-28 16:47:27 -0300

    move funtion to right folder

--- services/route_service.py
+++ services/route_service.py
@@ -1,4 +1,4 @@
-from models import SegmentGenerationConfig, RouteSegment
+from models import SegmentGenerationConfig, RouteSegmentsGroupedByDistance, RouteSegmentGroupedByZoneAndTime
 from services.parse_files_service import parse_tcx_to_segmented_route
 from services.distance_service import convert_latlon_to_local_points
 from services.heading_service import compute_headings, smooth_headings
@@ -26,13 +26,13 @@ def create_segmented_route_from_route_file(segment_generation_config: SegmentGen
 
     # Check if we reached the minimum distance threshold or last point
     if acc_distance >= segment_generation_config.min_distance or i == len(local_points) - 1:
-      avg_grade = acc_weighted_grade / acc_distance
+      weighted_avg_grade = acc_weighted_grade / acc_distance
       heading = smoothed_headings[i - 1]
-      zone_name = get_zone_by_grade(avg_grade)
+      zone_name = get_zone_by_grade(weighted_avg_grade)
 
-      route_segment = RouteSegment(
+      route_segment = RouteSegmentsGroupedByDistance(
         distance=acc_distance,
-        grade=avg_grade,
+        grade=weighted_avg_grade,
         heading=heading,
         zone=zone_name
       )
@@ -44,3 +44,43 @@ def create_segmented_route_from_route_file(segment_generation_config: SegmentGen
       start_index = i
 
   return route
+
+def group_segments_by_zone_and_time(segments, min_duration=0.5, min_segments=5):
+  merged = []
+  current_zone = segments[0].zone
+  total_time = 0
+  total_power = 0
+  buffer = []
+
+  for segment in segments:
+    zone = segment.zone
+    power = segment.power_w
+    time_min = segment.time_min
+
+    if zone == current_zone and (total_time < min_duration or len(buffer) < min_segments):
+      # same zone, accumulate
+      buffer.append(segment)
+      total_time += time_min
+      total_power += power
+    else:
+      merged.append(RouteSegmentGroupedByZoneAndTime(
+        zone=current_zone,
+        power_w=total_power / len(buffer),
+        time_min=total_time
+      ))
+
+      # reset accumulators
+      current_zone = zone
+      total_time = time_min
+      total_power = power
+      buffer = [segment]
+
+  # commit the last buffer
+  if buffer:
+    merged.append(RouteSegmentGroupedByZoneAndTime(
+      zone=current_zone,
+      power_w=total_power / len(buffer),
+      time_min=total_time
+    ))
+
+  return merged
