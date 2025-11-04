0dcb9959c448fe681412f2917a7c8c9404b2880c
rodrigoRolim <rodrigorolimveras92@gmail.com>
2025-11-04 15:35:10 -0300

    add middleware cors to api

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes import api
 import logging
 
@@ -14,6 +15,14 @@ app = FastAPI(title="Workout generator Service")
 
 app.include_router(api.router, prefix="/workout")
 
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:3000"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+  expose_headers=["Content-Disposition"],
+)
 @app.get("/")
 def health_check():
   return {"message": "Api is running"}
